e9f1766c74b1b312e52107a7ffbd302a00f2ee75
Ferdous-M <mahzabinferdous000@gmail.com>
2026-09-17 15:41:19 +0600

    Update views.py

--- companyapi/views.py
+++ companyapi/views.py
@@ -1,16 +1,16 @@
-# from django.http import HttpResponse
-
-# def home_page(request):
-#     print("home page requested")
-#     return HttpResponse("This is a home page")
-
-
-from django.http import JsonResponse
-
+from django.http import HttpResponse
 
 def home_page(request):
     print("home page requested")
+    return HttpResponse("<h1>This is a home page</h1>")
 
-    return JsonResponse({
-        "message": "This is our company API"
-    })
+
+# from django.http import JsonResponse
+
+
+# def home_page(request):
+#     print("home page requested")
+
+#     return JsonResponse({
+#         "message": "This is our company API"
+#     })
