67254b8089df7190ae9f85edf2fe2149dd3b9934
Ferdous-M <mahzabinferdous000@gmail.com>
2026-09-17 19:19:45 +0600

    Update seriliazers.py

--- api/seriliazers.py
+++ api/seriliazers.py
@@ -7,3 +7,11 @@ class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Company
         fields = "__all__"
+
+
+                
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    id=serializers.ReadOnlyField()    
+    class Meta:
+        model=Employee
+        fields="__all__"
