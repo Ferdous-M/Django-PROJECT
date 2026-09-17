3aa8e39fdba742f9d6824f26cd8452fe19a71c03
Ferdous-M <mahzabinferdous000@gmail.com>
2026-09-17 16:15:45 +0600

    Update urls.py

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from api.views import CompanyViewSet,EmployeeViewSet
+from rest_framework import routers
+
+
+router= routers.DefaultRouter()
+router.register(r'companies', CompanyViewSet)
